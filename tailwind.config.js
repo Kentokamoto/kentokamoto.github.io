d0e60d26e82f2030c31fd03e86becb9526414fe9
Kento Okamoto <kentokamoto@protonmail.com>
2022-12-19 09:54:56 -0800

    Colors and layouts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}",
-    './node_modules/tw-elements/dist/js/**/*.js'],
+  content: ["./**/*.{html,js}", "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "pewter": "#D7E3E5",
+        "misty-blue": "#2E4D58",
+        "baby-blue": "#3D9ABD",
+        "blue-grotto": "#00789D",
+      },
+    },
   },
-  plugins: [
-        require('tw-elements/dist/plugin')
-    ],
-}
+  plugins: [require("tw-elements/dist/plugin")],
+};
