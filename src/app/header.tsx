af5451e6a908a6c75ace90f3676e1932a78c2c41
Kento Okamoto <kentokamoto@proton.me>
2025-02-01 22:39:45 -0800

    Slide in hamburger menu

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,23 +1,17 @@
 'use client'
-import { useState } from 'react'
-
-export default function Header() {
-    const [isOpen, setIsOpen] = useState(false)
-    const handleOpen = () => {
-        setIsOpen(!isOpen)
-    }
+export default function Header({ isOpen, onOpen }) {
     return (
         <header className="flex flex-row-reverse sm:flex-row text-4xl md:text-2xl">
             <div className="hidden sm:flex px-4 py-3 md:py-4 ">About</div>
             <button
-                className="flex flex-col sm:hidden px-4 py-3"
-                onClick={handleOpen}
+                className="flex flex-col sm:hidden px-4 py-3 z-50"
+                onClick={onOpen}
             >
                 <span
                     className={`block w-7 h-1 m-0.75 rounded-xs bg-white origin-top-left ${isOpen ? 'duration-300  rotate-45 translate-y-1/5' : 'duration-300'}`}
                 ></span>
                 <span
-                    className={`block w-7 h-1 m-0.75 rounded-xs ${isOpen ? 'duration-300  bg-black' : 'duration-300 bg-white'}`}
+                    className={`block w-7 h-1 m-0.75 rounded-xs ${isOpen ? 'duration-300  bg-black ' : 'duration-300 bg-white'}`}
                 ></span>
                 <span
                     className={`block w-7 h-1 m-0.75 rounded-xs bg-white origin-bottom-left ${isOpen ? 'duration-300  -rotate-45 -translate-y-1/5' : 'duration-300'}`}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+'use client'
+import { useState } from 'react'
 import Header from './header'
 export default function Home() {
+    const [isOpen, setIsOpen] = useState(false)
+    const handleOpen = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <div>
-            <Header />
+            <Header isOpen={isOpen} onOpen={handleOpen} />
+            <div
+                className={`absolute h-screen top-0 left-0 duration-300 bg-black w-screen ease-in-out ${isOpen ? '' : '-translate-y-full'}`}
+            >
+                <div>About</div>
+            </div>
             <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-                <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
+                <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                    <p>&#x1F6A7; Check back later &#x1F6A7;</p>
+                </main>
                 <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
             </div>
         </div>
