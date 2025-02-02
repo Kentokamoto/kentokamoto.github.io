9a75afc972d92fa9edc69f04ae3d3592b994461b
Kento Okamoto <kentokamoto@proton.me>
2025-02-02 10:25:31 -0800

    change minimum width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${geistSans.variable} ${geistMono.variable} min-w-sm antialiased`}
+                className={`${geistSans.variable} ${geistMono.variable} min-w-xs antialiased`}
             >
                 {children}
             </body>
