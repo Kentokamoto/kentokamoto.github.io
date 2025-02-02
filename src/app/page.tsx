28b09b17e7203257fcd7e41136967b9789407a62
Kento Okamoto <kentokamoto@proton.me>
2025-02-02 10:22:05 -0800

    fix small screen issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,14 @@ export default function Home() {
     }
 
     return (
-        <div>
-            <Header isOpen={isOpen} onOpen={handleOpen} />
+        <div className={'h-screen w-screen'}>
             <div
                 className={`absolute top-0 left-0 h-screen w-screen bg-white text-4xl duration-300 ease-in-out dark:bg-black ${isOpen ? 'opacity-100' : '-translate-y-5 opacity-0'}`}
             >
                 <div className="px-4 py-3 sm:flex md:py-4">About</div>
             </div>
+            <Header isOpen={isOpen} onOpen={handleOpen} />
+
             <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
                 <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
                     <p>&#x1F6A7; Check back later &#x1F6A7;</p>
