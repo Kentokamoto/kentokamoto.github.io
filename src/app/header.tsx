899dbd4965236c6e8e847119d17dca39343651fb
Kento Okamoto <kentokamoto@proton.me>
2025-02-02 14:16:41 -0800

    Move navbar into component

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,45 +1,11 @@
 'use client'
+import NavBar from '@/components/NavBar'
 import React from 'react'
 
-interface HeaderProps {
-    isOpen: boolean
-    onOpen: () => void
-}
-
-const Header: React.FC<HeaderProps> = ({ isOpen, onOpen }) => {
+const Header = () => {
     return (
         <header className="flex flex-row-reverse text-4xl sm:flex-row md:text-2xl">
-            <div
-                className={`bg-nord4 dark:bg-nord0 absolute top-0 left-0 flex h-screen w-screen flex-col place-content-center text-4xl duration-300 ease-in-out ${isOpen ? 'opacity-100' : '-translate-y-5 opacity-0'}`}
-            >
-                <div className="px-4 py-3 text-right sm:flex md:py-4">
-                    About
-                </div>
-                <div className="px-4 py-3 text-right sm:flex md:py-4">
-                    Posts
-                </div>
-                <div className="px-4 py-3 text-right sm:flex md:py-4">
-                    Resume
-                </div>
-            </div>
-
-            <div className="hidden px-4 py-3 sm:flex md:py-4">About</div>
-            <div className="hidden px-4 py-3 sm:flex md:py-4">Posts</div>
-            <div className="hidden px-4 py-3 sm:flex md:py-4">Resume</div>
-            <button
-                className="z-50 flex flex-col px-4 py-3 sm:hidden"
-                onClick={onOpen}
-            >
-                <span
-                    className={`m-0.75 block h-1 w-7 origin-top-left rounded-xs bg-black dark:bg-white ${isOpen ? 'translate-x-1/5 translate-y-1/5 rotate-45 duration-300' : 'duration-300'}`}
-                ></span>
-                <span
-                    className={`m-0.75 block h-1 w-7 rounded-xs ${isOpen ? 'invisible duration-300' : 'bg-black duration-300 dark:bg-white'}`}
-                ></span>
-                <span
-                    className={`m-0.75 block h-1 w-7 origin-bottom-left rounded-xs bg-black dark:bg-white ${isOpen ? '-translate-y-1/5 translate-x-1/5 -rotate-45 duration-300' : 'duration-300'}`}
-                ></span>
-            </button>
+            <NavBar />
         </header>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,9 @@
 'use client'
-import { useState } from 'react'
 import Header from './header'
 export default function Home() {
-    const [isOpen, setIsOpen] = useState(false)
-    const handleOpen = () => {
-        setIsOpen(!isOpen)
-    }
-
     return (
         <div>
-            <div
-                className={`bg-nord4 dark:bg-nord0 absolute top-0 left-0 flex h-screen w-screen flex-col place-content-center text-4xl duration-300 ease-in-out ${isOpen ? 'opacity-100' : '-translate-y-5 opacity-0'}`}
-            >
-                <div className="px-4 py-3 text-right sm:flex md:py-4">
-                    About
-                </div>
-                <div className="px-4 py-3 text-right sm:flex md:py-4">
-                    Posts
-                </div>
-                <div className="px-4 py-3 text-right sm:flex md:py-4">
-                    Resume
-                </div>
-            </div>
-            <Header isOpen={isOpen} onOpen={handleOpen} />
+            <Header />
 
             <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
                 <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
