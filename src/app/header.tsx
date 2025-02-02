07583545ee35c9289bfd3a93474aa9fb7f3853a7
Kento Okamoto <kentokamoto@proton.me>
2025-02-01 23:35:54 -0800

    prettier tailwindcss

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,22 +1,31 @@
 'use client'
-export default function Header({ isOpen, onOpen }) {
+import React from 'react'
+
+interface HeaderProps {
+    isOpen: boolean
+    onOpen: () => void
+}
+
+const Header: React.FC<HeaderProps> = ({ isOpen, onOpen }) => {
     return (
-        <header className="flex flex-row-reverse sm:flex-row text-4xl md:text-2xl">
-            <div className="hidden sm:flex px-4 py-3 md:py-4 ">About</div>
+        <header className="flex flex-row-reverse text-4xl sm:flex-row md:text-2xl">
+            <div className="hidden px-4 py-3 sm:flex md:py-4">About</div>
             <button
-                className="flex flex-col sm:hidden px-4 py-3 z-50"
+                className="z-50 flex flex-col px-4 py-3 sm:hidden"
                 onClick={onOpen}
             >
                 <span
-                    className={`block w-7 h-1 m-0.75 rounded-xs bg-white origin-top-left ${isOpen ? 'duration-300  rotate-45 translate-y-1/5' : 'duration-300'}`}
+                    className={`m-0.75 block h-1 w-7 origin-top-left rounded-xs bg-white ${isOpen ? 'translate-y-1/5 rotate-45 duration-300' : 'duration-300'}`}
                 ></span>
                 <span
-                    className={`block w-7 h-1 m-0.75 rounded-xs ${isOpen ? 'duration-300  bg-black ' : 'duration-300 bg-white'}`}
+                    className={`m-0.75 block h-1 w-7 rounded-xs ${isOpen ? 'invisible duration-300' : 'bg-white duration-300'}`}
                 ></span>
                 <span
-                    className={`block w-7 h-1 m-0.75 rounded-xs bg-white origin-bottom-left ${isOpen ? 'duration-300  -rotate-45 -translate-y-1/5' : 'duration-300'}`}
+                    className={`m-0.75 block h-1 w-7 origin-bottom-left rounded-xs bg-white ${isOpen ? '-translate-y-1/5 -rotate-45 duration-300' : 'duration-300'}`}
                 ></span>
             </button>
         </header>
     )
 }
+
+export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,15 @@ export default function Home() {
         <div>
             <Header isOpen={isOpen} onOpen={handleOpen} />
             <div
-                className={`text-4xl absolute h-screen top-0 left-0 bg-black duration-300 w-screen ease-in-out ${isOpen ? 'opacity-100' : 'opacity-0 -translate-y-5'}`}
+                className={`absolute top-0 left-0 h-screen w-screen bg-black text-4xl duration-300 ease-in-out ${isOpen ? 'opacity-100' : '-translate-y-5 opacity-0'}`}
             >
-                <div className="sm:flex px-4 py-3 md:py-4">About</div>
+                <div className="px-4 py-3 sm:flex md:py-4">About</div>
             </div>
-            <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-                <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+            <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+                <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
                     <p>&#x1F6A7; Check back later &#x1F6A7;</p>
                 </main>
-                <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+                <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6"></footer>
             </div>
         </div>
     )
