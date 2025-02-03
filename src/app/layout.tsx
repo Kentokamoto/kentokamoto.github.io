1f4f34696b73756563fcf7ee660dae5eb2e3147b
Kento Okamoto <kentokamoto@proton.me>
2025-02-02 17:35:20 -0800

    import react

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Header from './header'
 import Footer from './footer'
+import React from 'react'
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
