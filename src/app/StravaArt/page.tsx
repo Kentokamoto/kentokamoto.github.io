bf9ad68a47b8fcd975b4c7a24fffa885de21ca61
Kento Okamoto <kentokamoto@proton.me>
2025-02-09 15:27:57 -0800

    Cleanup Strava Activity Component

--- src/app/StravaArt/page.tsx
+++ src/app/StravaArt/page.tsx
@@ -1,17 +1,20 @@
 'use client'
 
-import StravaActivities from '@/components/StravaActivities'
+import StravaActivity from '@/components/StravaActivity'
 
 export default function StravaArt() {
     const blurb = ' This is a test'
-
+    const activityIds = [12925466896, 12862826754]
     return (
         <div className="flex flex-col p-8 pb-20">
             <div>
                 <p>{blurb}</p>
             </div>
-            <div className="flex flex-row flex-wrap items-center justify-center"></div>
-            <StravaActivities />
+            <div className="flex flex-row flex-wrap items-center justify-center">
+                {activityIds.map((activityId) => (
+                    <StravaActivity activityId={activityId} key={activityId} />
+                ))}
+            </div>
         </div>
     )
 }
--- src/components/StravaActivity/index.tsx
+++ src/components/StravaActivity/index.tsx
@@ -1,5 +1,4 @@
 import Script from 'next/script'
-import React from 'react'
 const StravaActivity = ({ activityId }: { activityId: number }) => {
     return (
         <>
